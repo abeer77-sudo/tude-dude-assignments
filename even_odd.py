12fc7785f7965f0588d750a469ccfac269a8c7bb
abeer77-sudo <abeerchawla31@gmail.com>
2025-08-16 21:41:59 +0530

    Add files via upload

---
+++ even_odd.py
@@ -0,0 +1,5 @@
+n1=int(input("Enter first number: "))
+if n1 % 2 == 0:
+    print(f'{n1} is even')
+else:
+    print(f'{n1} is odd')
---
+++ for_sum.py
@@ -0,0 +1,3 @@
+
+r=sum(i for i in range(1,51))
+print(f'sum of numbers from 1 to 51 is:{r}')
